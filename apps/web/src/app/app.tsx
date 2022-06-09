93968cbcf621c7855f814c4e431b3280d9623b70
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-06-09 12:45:24 +0300

    modified component

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,13 +1,16 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
-
-export function App() {
+export interface AppProps {
+  title: string;
+  rating: number;
+  flag: boolean;
+}
+export function App(props: AppProps) {
   return (
-    <>
-      <NxWelcome title="web" />
-      <div />
-    </>
+    <div>
+      <h1>React app using Nx</h1>
+      <p>Title: {props.title}</p>
+      <p>Rating: {props.rating}</p>
+      <p>Flag: {props.flag ? 'true' : 'false'}</p>
+    </div>
   );
 }
 
